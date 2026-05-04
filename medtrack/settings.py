46edafe16df9d2728e036fff3c3a1c35e2afdb36
Nelson Molokwu <molokwunel94@gmail.com>
2026-05-04 19:45:27 +0100

    Add filtering, search, ordering and pagination

--- medtrack/settings.py
+++ medtrack/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'patients',
 ]
 
@@ -120,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#filtering and searching
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
--- patients/views.py
+++ patients/views.py
@@ -1,8 +1,13 @@
 from patients.models import Patients
 from patients.serializers import PatientSerializers
 from rest_framework import viewsets #importing viewsets which handles GET, POST, PUT, DELETE requests
+from django_filters.rest_framework import DjangoFilterBackend #importing DjangoFilterBackend for filtering the queryset based on specified fields
+from rest_framework import filters #importing filters for search functionality
 
 # Create your views class here.
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patients.objects.all() #queryset to retrieve all patient records
+    filterset_fields = ['gender', 'blood_group', 'genotype']
+    search_fields = ['first_name', 'last_name']
+    ordering_fields = ['created_at', 'date_of_birth', 'weight_kg', 'temperature_celsius', 'blood_pressure_systolic']
     serializer_class = PatientSerializers #specifying the serializer class to use for this viewset
